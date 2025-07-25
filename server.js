49727aef36a737701a492583076efddd7226ed6c
ivanhsiaj <jaishnavitirumanagiri24@gmail.com>
2025-07-25 11:07:08 -0400

    ping added

--- server.js
+++ server.js
@@ -36,5 +36,9 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/admin', adminRoutes);
 app.use('/api/materials', materialRoutes);
 app.use('/api/discharge', dischargeRoutes);
+app.get("/api/ping", (req, res) => {
+  console.log("pong");
+  res.send("pong");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
